aacc66b2058996d429436f820f832bf8b7a7abaf
Messiasbbs <messiasbsbdev@gmail.com>
2024-01-31 16:29:55 -0300

    Initial Commit

---
+++ ProjetoFinal/ProjetoFinalApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjetoFinalApp.swift
+//  ProjetoFinal
+//
+//  Created by Turma02-19 on 31/01/24.
+//
+
+import SwiftUI
+
+@main
+struct ProjetoFinalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
